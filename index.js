4b9a05eaaf150b34db14fc889add83c49a1d2786
Nguyễn Ngọc Truyện <truyen027@gmail.com>
2023-05-01 16:40:26 +0700

    CRUD in Mongodb

--- index.js
+++ index.js
@@ -1,45 +1,113 @@
 const mongoose = require('mongoose');
 
-let url = 'mongodb+srv://nntruyen027:2706@cluster0.exukcih.mongodb.net/?retryWrites=true&w=majority';
+const db = mongoose.connect('mongodb://localhost:27017/StudentManagement', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connecting!');
+}).catch(err => {
+    console.error(err);
+})
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-var db = mongoose.connection;
-
-db.on('error', (err) => {
-    if (err) throw err;
+let studentSchema = new mongoose.Schema({
+    name: {
+        firstName: String,
+        lastName: String
+    },
+    man: Boolean,
+    major: String,
+    course: Number,
+    faculty: String
 });
 
-db.once('open', () => {
-    //Khởi tạo schema
-    var Schema = mongoose.Schema;
+let Student = mongoose.model('student', studentSchema);
 
-    var blogSchema = new Schema({
-        title: String,
-        author: String,
-        body: String,
-        hidden: Boolean
-    });
+// Student.insertMany([
+//     {
+//         name: {
+//             firstName: 'Nguyễn Ngọc',
+//             lastName: 'Truyện'
+//         },
+//         man: true,
+//         major: 'Software Engineering',
+//         course: 46,
+//         faculty: 'College Of Infomation And Comunication Technology'
+//     },
+//     {
+//         name: {
+//             firstName: 'Đỗ Kỳ',
+//             lastName: 'Duyên'
+//         },
+//         man: false,
+//         major: 'Software Engineering',
+//         course: 46,
+//         faculty: 'College Of Infomation And Comunication Technology'
+//     },
+//     {
+//         name: {
+//             firstName: 'Bạch Toàn',
+//             lastName: 'Mỹ'
+//         },
+//         man: true,
+//         major: 'Computer Networking And Data Comunicaion',
+//         course: 46,
+//         faculty: 'College Of Infomation And Comunication Technology'
+//     },
+//     {
+//         name: {
+//             firstName: 'Nguyễn Phúc Nguyên',
+//             lastName: 'Khoa'
+//         },
+//         man: true,
+//         major: 'Software Engineering',
+//         course: 47,
+//         faculty: 'College Of Infomation And Comunication Technology'
+//     },
+//     {
+//         name: {
+//             firstName: 'Đặng Minh',
+//             lastName: 'Nhựt'
+//         },
+//         man: true,
+//         major: 'Computer Engineering',
+//         course: 47,
+//         faculty: 'College Of Engineering'
+//     },
+//     {
+//         name: {
+//             firstName: 'Lê Nguyễn Anh Tuấn',
+//             lastName: 'Nhựt'
+//         },
+//         man: true,
+//         major: 'Construction Engineering',
+//         course: 47,
+//         faculty: 'College Of Engineering'
+//     },
+//     {
+//         name: {
+//             firstName: 'Huỳnh Long',
+//             lastName: 'Vân'
+//         },
+//         man: true,
+//         major: 'Computer Networking And Data Comunicaion',
+//         course: 47,
+//         faculty: 'College Of Infomation And Comunication Technology'
+//     }
+// ]).catch(err => {
+//     console.log(err);
+// })
 
-    blogSchema.methods.showMessages = function () {
-        console.log(`Đã thêm bài viết mới có tên ${this.title}`);
-    }
 
-    var Blog = mongoose.model('Blog', blogSchema);
-
-    var dataInsert = {
-        title: 'Lập trình NodeJS căn bản',
-        author: 'Freetuts.net',
-        body: 'Nội dung lập trình NodeJs căn bản',
-        hidden: false
-    }
-
-    var blogCollections = new Blog(dataInsert);
-
-    blogCollections.save().then(data => {
-        console.log(data);
-        blogCollections.showMessages();
-    }).catch(err => {
-        console.error(err);
+Student.find({ course: 47, faculty: 'College Of Engineering' }, 'name major').then(result => {
+    result.forEach(student => {
+        console.log(`Student: ${student.name.firstName} ${student.name.lastName} - Major: ${student.major}`);
     })
-});
+})
+
+Student.findOneAndUpdate({ name: { firstName: 'Lê Nguyễn Anh Tuấn', lastName: 'Nhựt' } }, { name: { firstName: 'Lê Nguyễn Anh', lastName: 'Tuấn' } }, { new: true }).then(result => {
+    console.log(result);
+}).catch(err => {
+    console.error(err);
+})
+
 
